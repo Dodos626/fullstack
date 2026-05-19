88a4fcd0a870e9a258198a79ff34988edda24111
Dodos626 <chlktheo@gmail.com>
2026-05-19 19:27:47 +0300

    added copyright lmao

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -1,5 +1,9 @@
 import styles from './footer.module.css';
 
 export const Footer = () => {
-    return <div className={styles.footerBody}></div>;
+    return (
+        <div className={styles.footerBody}>
+            <div className={styles.footerText}>© 2026 Theodoros Chalkidis</div>
+        </div>
+    );
 };
